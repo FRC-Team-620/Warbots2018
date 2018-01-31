3330967812c2281341ceee98a9275161950d9299
Chroniaro <zachary.carlini@gmail.com>
2018-01-31 17:57:44 -0500

    Added CalibrateDriveTrain module

---
+++ src/org/jmhsrobotics/modules/CalibrateDriveTrain.java
@@ -0,0 +1,31 @@
+package org.jmhsrobotics.modules;
+
+import org.jmhsrobotics.core.modulesystem.CommandModule;
+import org.jmhsrobotics.core.modulesystem.Submodule;
+import org.jmhsrobotics.hardwareinterface.Drive;
+import org.jmhsrobotics.hardwareinterface.WheelEncodersInterface;
+
+public class CalibrateDriveTrain extends CommandModule
+{
+	private @Submodule Drive driveTrain;
+	private @Submodule WheelEncodersInterface encoders;
+	
+	@Override
+	protected void initialize()
+	{
+	}
+	
+	@Override
+	protected void execute()
+	{
+		encoders.left().getRate(); //get speed of left pid
+		encoders.average().getDist(); //get average dist of both encoders
+		driveTrain.drive(0.5, 0.5); //drive the bot
+	}
+	
+	@Override
+	protected boolean isFinished()
+	{
+		return false;
+	}
+}
