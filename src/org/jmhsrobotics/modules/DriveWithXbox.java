ce6b380d78a414c33f36f33c27579b5de85d003e
Team X <Team X@10.122.190.62>
2018-02-01 19:02:09 -0500

    Added Xbox driving

--- src/org/jmhsrobotics/modules/DriveWithXbox.java
+++ src/org/jmhsrobotics/modules/DriveWithXbox.java
@@ -7,22 +7,42 @@ import org.jmhsrobotics.core.modulesystem.ControlSchemeModule;
 import org.jmhsrobotics.core.modulesystem.DriveController;
 import org.jmhsrobotics.core.modulesystem.Submodule;
 
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class DriveWithXbox extends ControlSchemeModule
 {
 	private @Submodule Optional<SubsystemManager> subsystems;
 	private @Submodule DriveController drive;
-	
+
 	@Override
 	public void onLink()
 	{
 		subsystems.ifPresent(s -> requires(s.getSubsystem("DriveTrain")));
 	}
-	
+
 	@Override
 	public void execute()
 	{
 		XboxController xbox = getOI().getXboxControllers().get(0);
+
+		double x = xbox.getX(Hand.kLeft);
+		double y = xbox.getY(Hand.kLeft);
+		double xadjusted = 0;
+		double yadjusted = 0;
+		if (x > 0) {
+		xadjusted = (x -.2) / .8;
+		}
+		if (x < 0) {
+		xadjusted = (x +.2) / .8;
+		if (y > 0) {
+		yadjusted = (y -.2) / .8;
+				}
+		if (y < 0) {
+		yadjusted = (y +.2) / .8;
+					}
+		
+			drive.drive(yadjusted, xadjusted);
+		}
 	}
 }
