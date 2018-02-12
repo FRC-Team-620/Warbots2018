8f07afce36f6b5aeb365c07e580ca9046015a0fb
Chroniaro <zachary.carlini@gmail.com>
2018-02-12 17:41:56 -0500

    Added DriftTest command. DO NOT USE if you don't know what it does.

---
+++ src/org/jmhsrobotics/modules/testcommands/DriftTest.java
@@ -0,0 +1,37 @@
+package org.jmhsrobotics.modules.testcommands;
+
+import org.jmhsrobotics.core.modules.SubsystemManager;
+import org.jmhsrobotics.core.modulesystem.DriveController;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriftTest extends Command
+{
+	private DriveController drive;
+	
+	public DriftTest(DriveController drive)
+	{
+		this.drive = drive;
+	}
+	
+	public DriftTest(DriveController drive, SubsystemManager subsystems)
+	{
+		this(drive);
+		requires(subsystems.getSubsystem("DriveTrain"));
+	}
+	
+	@Override
+	protected void execute()
+	{
+		if(timeSinceInitialized() < 2)
+			drive.drive(1, 0);
+		else
+			drive.drive(0, 1);
+	}
+	
+	@Override
+	protected boolean isFinished()
+	{
+		return timeSinceInitialized() > 3;
+	}
+}
