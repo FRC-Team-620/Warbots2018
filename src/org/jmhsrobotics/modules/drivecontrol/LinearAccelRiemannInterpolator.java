de7547d7fb62c2d50bbc3e44da4a376d57f3d73a
mihirium237 <mihir@duggl.com>
2018-02-17 17:23:47 -0500

    It's ugly but it works

--- src/org/jmhsrobotics/modules/drivecontrol/LinearAccelRiemannInterpolator.java
+++ src/org/jmhsrobotics/modules/drivecontrol/LinearAccelRiemannInterpolator.java
@@ -12,12 +12,23 @@ public class LinearAccelRiemannInterpolator implements PositionInterpolator
 	}
 	
 	@Override
-	public Point getRelativePosition(double w0, double wt, double theta0, double thetat, double v0, double vt, double s, double dt)
+	public Point getRelativePosition(double wo, double wt, double thetao, double thetat, double vo, double vt, double s, double dt)
 	{
-		double xval = 0;
-		double yval = 0;
-		//for loop
+		double av = (-6*s+3*dt*(vt+vo))/(dt*dt*dt);
+		double bv = (6*s-2*dt*(vt+vo))/(dt*dt);
+		double cv = vo;
+		double at = (-6*(thetat-thetao)+3*dt*(wt+wo))/(dt*dt*dt);
+		double bt = (6*(thetat-thetao)-2*dt*(wt+wo))/(dt*dt);
+		double ct = wo;
+		double delta = dt/accuracy;
+		double xVal = 0;
+		double yVal = 0;
 		
-		return new Point(xval, yval);
+		for(int k=0;k<dt;k+=delta)
+		{
+			xVal += delta*(av*k*k+bv*k+cv)*Math.sin((1/3)*at*k*k*k+(1/2)*bt*k*k+ct*k+thetao);
+			yVal += delta*(av*k*k+bv*k+cv)*Math.cos((1/3)*at*k*k*k+(1/2)*bt*k*k+ct*k+thetao);
+		}
+		return new Point(xVal, yVal);
 	}
 }
