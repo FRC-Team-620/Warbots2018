afa40eff396ad6300241d0bb10a7d94634be8929
Chroniaro <zachary.carlini@gmail.com>
2018-02-17 19:24:08 -0500

    cleaned up reimann code

--- src/org/jmhsrobotics/modules/drivecontrol/LinearAccelRiemannInterpolator.java
+++ src/org/jmhsrobotics/modules/drivecontrol/LinearAccelRiemannInterpolator.java
@@ -5,30 +5,33 @@ import org.jmhsrobotics.core.util.Point;
 public class LinearAccelRiemannInterpolator implements PositionInterpolator
 {
 	private int accuracy;
-	
+
 	public LinearAccelRiemannInterpolator(int accuracy)
 	{
 		this.accuracy = accuracy;
 	}
-	
+
 	@Override
 	public Point getRelativePosition(double wo, double wt, double thetao, double thetat, double vo, double vt, double s, double dt)
 	{
-		double av = (-6*s+3*dt*(vt+vo))/(dt*dt*dt);
-		double bv = (6*s-2*dt*(vt+vo))/(dt*dt);
-		double cv = vo;
-		double at = (-6*(thetat-thetao)+3*dt*(wt+wo))/(dt*dt*dt);
-		double bt = (6*(thetat-thetao)-2*dt*(wt+wo))/(dt*dt);
-		double ct = wo;
-		double delta = dt/accuracy;
-		double xVal = 0;
-		double yVal = 0;
-		
-		for(double k=0;k<dt;k+=delta)
+		double av = (-6 * s + 3 * dt * (vt + vo)) / (dt * dt * dt);
+		double bv = (6 * s - 2 * dt * (vt + vo)) / (dt * dt);
+
+		double dtheta = thetat - thetao;
+		double at = (-2 * dtheta + dt * (wt + wo)) / (dt * dt * dt);
+		double bt = (3 * dtheta - dt * (wt + wo)) / (dt * dt);
+
+		double delta = dt / accuracy;
+		double x = 0;
+		double y = 0;
+		for (double k = 0; k < dt; k += delta)
 		{
-			xVal += delta*(av*k*k+bv*k+cv)*Math.sin((1./3)*at*k*k*k+(1./2)*bt*k*k+ct*k+thetao);
-			yVal += delta*(av*k*k+bv*k+cv)*Math.cos((1./3)*at*k*k*k+(1./2)*bt*k*k+ct*k+thetao);
+			double v = (av * k * k + bv * k + vo);
+			double theta = at * k * k * k + bt * k * k + wo * k + thetao;
+			x += v * Math.sin(theta) * delta;
+			y += v * Math.cos(theta) * delta;
 		}
-		return new Point(xVal, yVal);
+
+		return new Point(x, y);
 	}
 }
